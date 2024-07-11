4ebbddc87c41dd7cea22f5829cfe65bc614cf5fd
Luan Verdelho <luanverdelho642@gmail.com>
2024-07-11 14:22:09 -0300

    chore: remove js files coming from dist

--- main/factories/traffic-light.factory.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeTrafficLight = void 0;
-var usecase_1 = require("../../application/usecase");
-var traffic_light_states_factory_1 = require("./traffic-light-states.factory");
-var makeTrafficLight = function () {
-    return new usecase_1.TrafficLight((0, traffic_light_states_factory_1.makeTrafficLightStates)());
-};
-exports.makeTrafficLight = makeTrafficLight;
